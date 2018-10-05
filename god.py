f3a8415cb38ade37e2cbcb24f6d9be86dd253d88
Vasilis Lyras <1708925@brunel.ac.uk>
2018-10-05 16:29:50 +0100

    Importing Stuff

---
+++ god.py
@@ -0,0 +1,51 @@
+print ("I am still a god")
+
+g = 50
+t = .27
+g += g * t
+
+#print (g)
+
+#voice = 'There\'s a snake in my boot'
+
+#print (voice + "!!!")
+
+#print (voice[1:3])
+#print (voice[:4])
+#print (len(voice))
+
+
+#name = 'bob'
+#age = 2
+#height = 2.55
+
+#print ("i am %s" % (name))
+#print ("i am %s, i am %d years old and %.2fm" % (name, age, height))
+
+#s = 'string'
+#print (s.upper())
+
+#name = input("whats yo name puto?")
+#number = int(input("aight aight, now tell me a number puto: "))
+
+#str(number)
+
+#elif = elseif statement
+
+#number = int(input("gimme a number: "))
+
+#if number<=100:
+#    print ("You can do better than that")
+#elif number<=1000:
+#    print ("almost")
+#elif number>1000:
+#    print ("There you go, you go girl")
+
+
+#oddoreven = int(input("gimme a number: "))
+
+
+#if oddoreven%2 == 0:
+#    print ("even")
+#else:
+#    print ("odd")
